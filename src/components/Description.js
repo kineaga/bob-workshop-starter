505a7cc81f1883833d43115357d0918ab59a564a
kineaga <kineaga@gmail.com>
2023-10-05 17:54:29 +0200

    Endret noe

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ function Description() {
   return (
     <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
       <Typography variant="h6" gutterBottom color="#3f51b5">
-        Om Meg
+        Kort oppsummert
       </Typography>
       <Typography>
       Austevollsbunaden er en vakker og tradisjonsrik drakt som stammer fra øykommunen Austevoll på Vestlandet i Norge. En viktig del av denne bunadens uttrykk er bringeduk, også kjent som bringeskjorte. Bringeduken til Austevollsbunaden er en nøye utformet og håndbrodert skjorte som bæres under livet og vesten. Denne skjorten er et mesterverk av bunadshåndverk og prydes ofte med vakre broderier som symboliserer regionens rike kulturarv. Bringeduken er med på å fullføre bunadens autentiske uttrykk, og den er en stolt bærer av Austevolls kulturarv og tradisjoner.
